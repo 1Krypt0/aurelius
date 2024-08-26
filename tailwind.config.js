7e0875e88cdb42585e0a67fee5b57451a7f1ad49
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2024-08-26 11:52:05 +0100

    Add tailwind and custom color

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				gold: '#b29e66'
+			}
+		}
+	},
+	plugins: []
+};
