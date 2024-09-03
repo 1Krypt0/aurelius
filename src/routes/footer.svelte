de9eea3bf6995a459c90bdeb4a03626ebbc05b68
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2024-09-03 16:07:44 +0100

    Make colors more coherent in header and footer

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,8 +1,4 @@
-<script lang="ts">
-	import { Facebook, Instagram, ChevronDown } from 'lucide-svelte';
-</script>
-
-<div class="flex items-center justify-between bg-primary py-3">
+<div class="flex items-center justify-between bg-primary py-3 text-foreground">
 	<div class="mx-auto flex flex-1 justify-center gap-16">
 		<a href="/about" class="hover:underline">About us</a>
 
@@ -18,7 +14,7 @@
 
 	<p class=" text-center">© 2024 Atomic. All Rights Reserved.</p>
 
-	<div class="mx-auto flex flex-1 justify-center gap-16 text-[#212B2A]">
+	<div class="mx-auto flex flex-1 justify-center gap-16">
 		<a href="/terms-conditions" class="hover:underline">Terms and Conditions</a>
 		<a href="/privacy-policy" class="hover:underline">Privacy Policy</a>
 	</div>
--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -4,7 +4,7 @@
 
 <nav class="flex items-center justify-between border-b border-b-border py-4">
 	<a
-		class="mx-auto flex flex-1 justify-center font-headers text-2xl hover:underline"
+		class="mx-auto flex flex-1 justify-center font-headers text-2xl text-foreground hover:underline"
 		href="/auctions"
 	>
 		Auctions
@@ -16,7 +16,7 @@
 
 	<a
 		href="/login"
-		class="mx-auto flex flex-1 items-center justify-center gap-4 font-headers text-2xl hover:underline"
+		class="mx-auto flex flex-1 items-center justify-center gap-4 font-headers text-2xl text-foreground hover:underline"
 	>
 		Log in/Sign up
 	</a>
