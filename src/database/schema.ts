2cb8af02abc6867fe7aa4083340173f3c765fca6
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2024-09-04 17:42:35 +0100

    Add db schema

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,8 +1,27 @@
-import { text, pgTable, timestamp, boolean, real, bigint } from 'drizzle-orm/pg-core';
+import { text, pgTable, timestamp, boolean, real, integer } from 'drizzle-orm/pg-core';
 
 export const userTable = pgTable('user', {
 	id: text('id').primaryKey(),
+	name: text('name').notNull(),
+	email: text('email').notNull().unique(),
+	password: text('password').notNull(),
+	isAdmin: boolean('is_admin').notNull().default(false)
 });
+
+export const productTable = pgTable('product', {
+	id: text('id').primaryKey(),
+	name: text('name').notNull(), // NOTE: Check if not unique
+	description: text('description').notNull(),
+	price: real('price').notNull(),
+	startDate: timestamp('start_date', { withTimezone: true }).notNull(),
+	endDate: timestamp('end_date', { withTimezone: true }).notNull()
+});
+
+export const bidTable = pgTable('bid', {
+	id: integer('id').primaryKey().generatedByDefaultAsIdentity({ startWith: 0 }),
+	value: real('value').notNull()
+});
+
 export const sessionTable = pgTable('session', {
 	id: text('id').primaryKey(),
 	userId: text('user_id')
