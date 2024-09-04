641d1928654f842b72f8132b0c6be194aeb26ee4
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2024-09-04 17:42:25 +0100

    Add lucia auth setup

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,5 +1,15 @@
-import { integer, pgTable } from 'drizzle-orm/pg-core';
+import { text, pgTable, timestamp, boolean, real, bigint } from 'drizzle-orm/pg-core';
 
-export const table = pgTable('test', {
-	int: integer('int')
+export const userTable = pgTable('user', {
+	id: text('id').primaryKey(),
+});
+export const sessionTable = pgTable('session', {
+	id: text('id').primaryKey(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => userTable.id),
+	expiresAt: timestamp('expires_at', {
+		withTimezone: true,
+		mode: 'date'
+	}).notNull()
 });
---
+++ src/lib/server/auth.ts
@@ -0,0 +1,32 @@
+import { DrizzlePostgreSQLAdapter } from '@lucia-auth/adapter-drizzle';
+import db from '../../database/drizzle';
+import { Lucia, TimeSpan } from 'lucia';
+import { dev } from '$app/environment';
+import { sessionTable, userTable } from '../../database/schema';
+
+const adapter = new DrizzlePostgreSQLAdapter(db, sessionTable, userTable);
+
+export const lucia = new Lucia(adapter, {
+	sessionExpiresIn: new TimeSpan(2, 'w'),
+	sessionCookie: {
+		attributes: {
+			secure: !dev
+		}
+	},
+	getUserAttributes(databaseUserAttributes) {
+		return {
+			name: databaseUserAttributes.name
+		};
+	}
+});
+
+declare module 'lucia' {
+	interface Register {
+		Lucia: typeof lucia;
+		DatabaseUserAttributes: DatabaseUserAttributes;
+	}
+}
+
+interface DatabaseUserAttributes {
+	name: string;
+}
