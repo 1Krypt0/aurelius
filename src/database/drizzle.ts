aa8b51f9d3412578ea238decd74cef97877311da
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2024-09-05 11:37:01 +0100

    Change to previous method of passing env var. Remove xata deps.

--- src/database/drizzle.ts
+++ src/database/drizzle.ts
@@ -1,4 +1,5 @@
 import { drizzle } from 'drizzle-orm/neon-http';
+import { NEON_DATABASE_URL } from '$env/static/private';
 import { neon } from '@neondatabase/serverless';
 
 // NOTE: Re-add when they get out of beta
@@ -6,7 +7,7 @@ import { neon } from '@neondatabase/serverless';
 //
 // const xata = new XataClient({ apiKey: XATA_API_KEY, branch: XATA_BRANCH });
 
-const sql = neon(process.env.NEON_DATABASE_URL!);
+const sql = neon(NEON_DATABASE_URL);
 const db = drizzle(sql);
 
 export default db;
