a98db8aaaba8b9d4cd2649b34ad71f4f916aa3da
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2024-09-06 15:05:11 +0100

    Make header responsive

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -3,25 +3,25 @@
 </script>
 
 <section class="flex items-center justify-between">
-	<h1 class="font-headers text-3xl font-bold">Last Minute Deals</h1>
+	<h1 class="font-headers text-2xl font-bold">Last Minute Deals</h1>
 	<a href="/auctions" class="self-end hover:underline">View all</a>
 </section>
 <Separator class="my-4" />
 
 <section class="flex items-center justify-between">
-	<h1 class="font-headers text-3xl font-bold">Hot Products</h1>
+	<h1 class="font-headers text-2xl font-bold">Hot Products</h1>
 	<a href="/auctions" class="self-end hover:underline">View all</a>
 </section>
 <Separator class="my-4" />
 
 <section class="flex items-center justify-between">
-	<h1 class="font-headers text-3xl font-bold">Latest Auctions</h1>
+	<h1 class="font-headers text-2xl font-bold">Latest Auctions</h1>
 	<a href="/auctions" class="self-end hover:underline">View all</a>
 </section>
 <Separator class="my-4" />
 
 <section class="flex items-center justify-between">
-	<h1 class="font-headers text-3xl font-bold">Upcoming Auctions</h1>
+	<h1 class="font-headers text-2xl font-bold">Upcoming Auctions</h1>
 	<a href="/auctions" class="self-end hover:underline">View all</a>
 </section>
 <Separator class="my-4" />
--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -2,6 +2,7 @@
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 	import { Button } from '$lib/components/ui/button';
 	import logo from '$lib/assets/Aurelius.svg';
+	import { Menu, Settings, UserIcon, LogOut } from 'lucide-svelte';
 
 	import type { User } from 'lucia';
 
@@ -9,18 +10,61 @@
 </script>
 
 <header class="flex justify-center border-b border-b-border py-4">
-	<nav class="flex w-4/5 items-center justify-between">
-		<div class="flex grow basis-0">
+	<nav class="flex w-4/5 items-center md:justify-between">
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger asChild let:builder>
+				<Button class="md:hidden" variant="link" builders={[builder]}>
+					<Menu />
+				</Button>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content>
+				<DropdownMenu.Item>
+					<a class="font-headers text-foreground" href="/auctions">Auctions</a>
+				</DropdownMenu.Item>
+				<DropdownMenu.Separator />
+				{#if user}
+					<DropdownMenu.Group>
+						<DropdownMenu.Sub>
+							<DropdownMenu.SubTrigger>
+								<p class="font-headers">{user.name}</p>
+							</DropdownMenu.SubTrigger>
+							<DropdownMenu.SubContent>
+								<DropdownMenu.Item>
+									<UserIcon class="mr-2 h-4 w-4" />
+									<span>Profile</span>
+								</DropdownMenu.Item>
+								<DropdownMenu.Item>
+									<Settings class="mr-2 h-4 w-4" />
+									<span>Settings</span>
+								</DropdownMenu.Item>
+								<DropdownMenu.Item>
+									<LogOut class="mr-2 h-4 w-4" />
+									<form action="/logout" method="post">
+										<button type="submit">Logout</button>
+									</form>
+								</DropdownMenu.Item>
+							</DropdownMenu.SubContent>
+						</DropdownMenu.Sub>
+					</DropdownMenu.Group>
+				{:else}
+					<DropdownMenu.Item>
+						<a href="/login" class="font-headers">Login/Sign Up</a>
+					</DropdownMenu.Item>
+				{/if}
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+
+		<div class="hidden grow basis-0 md:flex">
 			<a class="font-headers text-3xl text-foreground hover:underline" href="/auctions">
 				Auctions
 			</a>
 		</div>
 
-		<a href="/">
+		<a href="/" class="mx-auto">
 			<img src={logo} alt="Logo" class="h-28" />
 		</a>
 
-		<div class="flex grow basis-0 justify-end">
+		<div class="hidden grow basis-0 justify-end md:flex">
 			{#if user}
 				<DropdownMenu.Root>
 					<DropdownMenu.Trigger asChild let:builder>
