66104ad1245a824ee4489ae05ef5badea450bc5e
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2024-09-13 09:05:58 +0100

    Add showcase auctions to main page

---
+++ src/routes/(app)/+page.server.ts
@@ -0,0 +1,58 @@
+import { and, asc, count, desc, eq, gt, lt } from 'drizzle-orm';
+import db from '../../database/drizzle';
+import { bidTable, productTable } from '../../database/schema';
+import type { PageServerLoad } from './$types';
+
+export const SHOWCASE_LIMIT = 3;
+
+export const load: PageServerLoad = async () => {
+	const now = new Date();
+	const twoDaysFromNow = new Date(now.getTime() + 2 * 24 * 60 * 60 * 1000);
+	const twoDaysAgo = new Date(now.getTime() - 2 * 24 * 60 * 60 * 1000);
+
+	const hotProducts = await db
+		.select({
+			product: productTable,
+			bidCount: count(bidTable.id)
+		})
+		.from(productTable)
+		.where(gt(productTable.endDate, now))
+		.leftJoin(bidTable, eq(productTable.id, bidTable.productId))
+		.groupBy(productTable.id)
+		.orderBy((fields) => desc(fields.bidCount))
+		.limit(SHOWCASE_LIMIT);
+
+	const lastMinuteDeals = await db
+		.select()
+		.from(productTable)
+		.where(and(gt(productTable.endDate, now), lt(productTable.endDate, twoDaysFromNow)))
+		.orderBy(asc(productTable.endDate))
+		.limit(SHOWCASE_LIMIT);
+
+	const latestAuctions = await db
+		.select()
+		.from(productTable)
+		.where(
+			and(
+				lt(productTable.startDate, now),
+				gt(productTable.endDate, now),
+				gt(productTable.startDate, twoDaysAgo)
+			)
+		)
+		.orderBy(desc(productTable.startDate))
+		.limit(SHOWCASE_LIMIT);
+
+	const upcomingAuctions = await db
+		.select()
+		.from(productTable)
+		.where(gt(productTable.startDate, now))
+		.orderBy(asc(productTable.startDate))
+		.limit(SHOWCASE_LIMIT);
+
+	return {
+		hotProducts,
+		lastMinuteDeals,
+		latestAuctions,
+		upcomingAuctions
+	};
+};
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,27 +1,85 @@
 <script lang="ts">
 	import { Separator } from '$lib/components/ui/separator';
+	import type { PageServerData } from './$types';
+	import AuctionShowcase from './auctions/auction-showcase.svelte';
+
+	export let data: PageServerData;
+
+	const SHOWCASE_LIMIT = 3;
+
+	const { lastMinuteDeals, hotProducts, latestAuctions, upcomingAuctions } = data;
 </script>
 
-<section class="flex items-center justify-between">
-	<h1 class="font-headers text-2xl font-bold">Last Minute Deals</h1>
-	<a href="/auctions" class="self-end hover:underline">View all</a>
-</section>
-<Separator class="my-4" />
+<div class="flex flex-col gap-24">
+	{#if lastMinuteDeals.length >= SHOWCASE_LIMIT}
+		<section>
+			<div class="flex items-center justify-between">
+				<h1 class="font-headers text-4xl font-bold">Last Minute Deals</h1>
+				<a href="/auctions" class="self-end hover:underline">View all</a>
+			</div>
+			<Separator class="my-4" />
 
-<section class="flex items-center justify-between">
-	<h1 class="font-headers text-2xl font-bold">Hot Products</h1>
-	<a href="/auctions" class="self-end hover:underline">View all</a>
-</section>
-<Separator class="my-4" />
+			<div class="flex items-center justify-between gap-12">
+				{#each lastMinuteDeals as deal}
+					<div class="w-1/4">
+						<AuctionShowcase auction={deal} />
+					</div>
+				{/each}
+			</div>
+		</section>
+	{/if}
 
-<section class="flex items-center justify-between">
-	<h1 class="font-headers text-2xl font-bold">Latest Auctions</h1>
-	<a href="/auctions" class="self-end hover:underline">View all</a>
-</section>
-<Separator class="my-4" />
+	{#if hotProducts.length >= SHOWCASE_LIMIT}
+		<section>
+			<div class="flex items-center justify-between">
+				<h1 class="font-headers text-4xl font-bold">Hot Products</h1>
+				<a href="/auctions" class="self-end hover:underline">View all</a>
+			</div>
+			<Separator class="my-4" />
 
-<section class="flex items-center justify-between">
-	<h1 class="font-headers text-2xl font-bold">Upcoming Auctions</h1>
-	<a href="/auctions" class="self-end hover:underline">View all</a>
-</section>
-<Separator class="my-4" />
+			<div class="flex items-center justify-between gap-12">
+				{#each hotProducts as deal}
+					<div class="w-1/4">
+						<AuctionShowcase auction={deal.product} />
+					</div>
+				{/each}
+			</div>
+		</section>
+	{/if}
+
+	{#if latestAuctions.length >= SHOWCASE_LIMIT}
+		<section>
+			<div class="flex items-center justify-between">
+				<h1 class="font-headers text-4xl font-bold">Latest Auctions</h1>
+				<a href="/auctions" class="self-end hover:underline">View all</a>
+			</div>
+			<Separator class="my-4" />
+
+			<div class="flex items-center justify-between gap-12">
+				{#each latestAuctions as deal}
+					<div class="w-1/4">
+						<AuctionShowcase auction={deal} />
+					</div>
+				{/each}
+			</div>
+		</section>
+	{/if}
+
+	{#if upcomingAuctions.length >= SHOWCASE_LIMIT}
+		<section>
+			<div class="flex items-center justify-between">
+				<h1 class="font-headers text-4xl font-bold">Upcoming Auctions</h1>
+				<a href="/auctions" class="self-end hover:underline">View all</a>
+			</div>
+			<Separator class="my-4" />
+
+			<div class="flex items-center justify-between gap-12">
+				{#each upcomingAuctions as deal}
+					<div class="w-1/4">
+						<AuctionShowcase auction={deal} />
+					</div>
+				{/each}
+			</div>
+		</section>
+	{/if}
+</div>
