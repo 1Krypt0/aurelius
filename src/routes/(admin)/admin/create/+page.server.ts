a6962ea2b813d7b8f51548947c07b250c3d2ec13
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2024-09-16 16:08:41 +0100

    Add product creation with image support

--- src/routes/(admin)/admin/create/+page.server.ts
+++ src/routes/(admin)/admin/create/+page.server.ts
@@ -1,10 +1,18 @@
 import { createInsertSchema } from 'drizzle-zod';
 import type { Actions, PageServerLoad } from './$types';
-import { productTable } from '../../../../database/schema';
+import { imageTable, productTable } from '../../../../database/schema';
 import { z } from 'zod';
-import { fail, superValidate } from 'sveltekit-superforms';
+import { fail, setError, superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
-
+import { generateIdFromEntropySize } from 'lucia';
+import db from '../../../../database/drizzle';
+import { eq } from 'drizzle-orm';
+import { redirect } from '@sveltejs/kit';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { Resource } from 'sst';
+import { v4 as uuid } from 'uuid';
+//
 // NOTE: Maybe update the limit if not enough for good quality images
 const IMAGE_SIZE_LIMIT = 6_000_000;
 
@@ -43,12 +51,67 @@ export const actions: Actions = {
 	default: async ({ request }) => {
 		const form = await superValidate(request, zod(createAuctionSchema));
 
-		console.log(form.data);
-
 		if (!form.valid) {
 			return fail(400, { form });
 		}
 
-		console.log('FORM WAS VALID');
+		const { name, description, price, startDate, endDate, images, startMinutes, endMinutes } =
+			form.data;
+
+		const id = generateIdFromEntropySize(20);
+
+		const alreadyExists = await db
+			.select({ name: productTable.name })
+			.from(productTable)
+			.where(eq(productTable.name, name));
+
+		if (alreadyExists.length !== 0) {
+			return setError(form, 'name', 'Name already in use');
+		}
+
+		await db.insert(productTable).values({
+			id,
+			name,
+			description,
+			price,
+			startDate: new Date(`${startDate}T${startMinutes}`),
+			endDate: new Date(`${endDate}T${endMinutes}`)
+		});
+
+		const imageURLs = [];
+
+		for (const image of images) {
+			const command = new PutObjectCommand({
+				Key: uuid(),
+				Bucket: Resource.FileUploads.name
+			});
+
+			const url = await getSignedUrl(new S3Client({}), command);
+
+			const res = await fetch(url, {
+				body: image,
+				method: 'PUT',
+				headers: {
+					'Content-Type': image.type,
+					'Content-Disposition': `attachment; filename=${image.name}`
+				}
+			});
+
+			// TODO: Add error handling for when images fail
+			const imageURL = res.url.split('?')[0];
+
+			imageURLs.push(imageURL);
+		}
+
+		const associatedProduct = imageURLs.map((entry) => {
+			return {
+				url: entry,
+				productId: id
+			};
+		});
+
+		await db.insert(imageTable).values(associatedProduct);
+
+		return redirect(302, '/admin');
 	}
 };
