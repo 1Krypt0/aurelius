f825e0561d0a6144bcc2ada0175874b1a42548e2
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2024-09-24 11:11:01 +0100

    Update infra structure

--- infra/page.ts
+++ infra/page.ts
@@ -1,7 +1,7 @@
 import { bucket } from './storage';
 import { secret } from './secret';
 
-const page = new sst.aws.SvelteKit('Aurelius', {
+export const page = new sst.aws.SvelteKit('Aurelius', {
 	link: [bucket, secret.NeonUrl, secret.StripeSecret, secret.StripeWebhookSecret],
 	buildCommand: 'bun run build',
 	dev: {
@@ -11,16 +11,3 @@ const page = new sst.aws.SvelteKit('Aurelius', {
 		install: ['@node-rs/argon2']
 	}
 });
-
-const auctionTracker = new sst.aws.Cron('AuctionTracker', {
-	schedule: 'cron(0/15 * * * ? *)',
-	job: {
-		handler: './src/cron/auction.handler',
-		link: [secret.NeonUrl]
-	}
-});
-
-export const outputs = {
-	page,
-	auctionTracker
-};
